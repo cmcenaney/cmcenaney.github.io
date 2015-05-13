f457c62d9c0a03a4675d265849ddb362114bc13e
Colleen McEnaney <colleenmcenaney@gmail.com>
2015-05-13 01:24:42 -0400

    added icons

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ $(document).ready(function(){
 		$.each(data, function(i, item){
 			$('.container').append(
 				'<div class="item">'+
-					'<img src="'+item.img+'"></img>'+
-					'<h3 class="name"><a href="'+item.link+'" target="_blank">'+item.name+'</a></h3>'+
-					
+					'<a href="'+item.link+'" target="_blank">'+
+						'<img src="'+item.img+'"></img>'+
+						'<img class="icon" src="'+item.icon+'"></img>'+
+						'<h3 class="name">'+item.name+'</h3>'+
+					'</a>'+
 				'</div>'
 			);
 			console.log(item);
