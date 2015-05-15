79f2ada90376ce14341f675ce40834a7a04e732f
Colleen McEnaney <colleenmcenaney@gmail.com>
2015-05-14 23:48:44 -0400

    fixed spacing issues

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
 	$.getJSON('info.json', function(data){
-		$.each(data, function(i, item){
-			$('.container').append(
-				'<div class="item">'+
+		// $.each(data, function(i, item){
+		// 	$('.objects').append(
+		// 		'<div class="item">'+
+		// 			'<a href="'+item.link+'" target="_blank">'+
+		// 				'<div class="main-image">'+
+		// 					'<img src="'+item.img+'"></img>'+
+		// 				'</div>'+
+		// 				'<div class="label">'+
+		// 					'<img src="'+item.icon+'"></img>'+
+		// 					'<h3 class="name">'+item.name+'</h3>'+
+		// 				'</div>'+
+		// 			'</a>'+
+		// 		'</div>'	
+		// 	);
+		// });
+
+		$.each(data, function(i,item){
+			$('.wrap').append(
+				'<div class="box">'+
 					'<a href="'+item.link+'" target="_blank">'+
-						'<img src="'+item.img+'"></img>'+
-						'<img class="icon" src="'+item.icon+'"></img>'+
-						'<h3 class="name">'+item.name+'</h3>'+
+						'<div class="boxInner">'+
+							'<img class="main-image" src="'+item.img+'"></img>'+
+							'<div class="titleBox">'+
+								'<img src="'+item.icon+'"/>'+
+								'<p>'+item.name+'</p>'+
+							'</div>'+
+						'</div>'+
 					'</a>'+
 				'</div>'
 			);
-			console.log(item);
 		});
 	});
 });
