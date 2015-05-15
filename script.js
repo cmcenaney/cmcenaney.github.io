bcfa06aa53d6ca92daee86364402c1051af9668b
Colleen McEnaney <colleenmcenaney@gmail.com>
2015-05-15 00:16:11 -0400

    added subhead

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 $(document).ready(function(){
 	$.getJSON('info.json', function(data){
-		// $.each(data, function(i, item){
-		// 	$('.objects').append(
-		// 		'<div class="item">'+
-		// 			'<a href="'+item.link+'" target="_blank">'+
-		// 				'<div class="main-image">'+
-		// 					'<img src="'+item.img+'"></img>'+
-		// 				'</div>'+
-		// 				'<div class="label">'+
-		// 					'<img src="'+item.icon+'"></img>'+
-		// 					'<h3 class="name">'+item.name+'</h3>'+
-		// 				'</div>'+
-		// 			'</a>'+
-		// 		'</div>'	
-		// 	);
-		// });
-
 		$.each(data, function(i,item){
-			$('.wrap').append(
+			$('.items').append(
 				'<div class="box">'+
 					'<a href="'+item.link+'" target="_blank">'+
 						'<div class="boxInner">'+
