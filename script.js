3dd8e6b1902fa880db9c5e3167aac49946cc3d03
Colleen McEnaney <colleenmcenaney@Colleens-Air.lan>
2023-01-23 23:57:56 -0500

    updating

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
+
+
 $(document).ready(function(){
-	$.getJSON('info.json', function(data){
-		$.each(data, function(i,item){
-			$('.items').append(
-				'<div class="col-md-4 box">'+
-					'<a href="'+item.link+'" target="_blank">'+
-						'<div class="boxInner">'+
-							'<div class="titleBox">'+
-								'<img src="'+item.icon+'"/>'+
-								'<p>'+item.name+'</p>'+
-							'</div>'+
-							'<img class="main-image" src="'+item.img+'"></img>'+
-						'</div>'+
-					'</a>'+
-				'</div>'
-			);
-		});
+	$.getJSON('projects.json', function(data){
+		
+		var d4c = _.filter(data, function(d){ return d.org == 'D4C' });
+		var wsj = _.filter(data, function(d) { return d.org == 'WSJ' });
+		var nyt = _.filter(data, function(d) { return d.org == 'NYT' });
+		var cc = _.filter(data, function(d) { return d.org == 'CC'});
+		var sd = _.filter(data, function(d) { return d.org == 'SD'});
+
+		var projects = {"sd": sd, "d4c": d4c, "wsj": wsj, "nyt": nyt, "cc": cc};
+
+		console.log(projects)
+
+		var template = $("#template").html();
+		var html = Mustache.to_html(template, projects);
+		$("#projects").html(html);
 	});
 
-	$.getJSON('resume.json', function(data){
-		$.each(data, function(i,item){
-			$('.resume-items').append(
-				'<div class="resume-box">'+
-					'<h2>'+item.place+'</h2>'+
-					'<p>'+item.position+'</p>'+
-					'<p>'+item.date+'</p>'+
-					'<ul>'+
-						'<li>'+item.desc1+'</li>'+
-						'<li>'+item.desc2+'</li>'+
-					'</ul>'+
-				'</div>'
-			);
-		});
-	});
+	
 });
